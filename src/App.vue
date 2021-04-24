33c441fd5121a326a0f59bdec4880d250aeccb14
kurdizle <kurdizle@gmail.com>
2021-04-25 05:20:38 +0900

    푸터 하단 고정

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,21 @@ export default {
 .container {
   padding: 50px 0;
 }
+
+html {
+  position: relative;
+  min-height: 100%;
+  margin: 0;
+}
+
+body {
+  min-height: 100%;
+}
+
+footer {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+}
 </style>
